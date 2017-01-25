ac5417517f272df9233ed5f89b0de227e3276576
Vinca Swanson <vswanson@shawscott.com>
2017-01-24 16:03:02 -0800

    index nav updates

--- index-nav.php
+++ index-nav.php
@@ -1,50 +1,25 @@
 <!--main nav-->
-<<<<<<< HEAD
+
 <div id="main-nav" class="navbar">
-	<div class="container-fluid">
-		<div class="navbar-header">
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
-				<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-			</button>
-			<a class="navbar-brand" href="#home"><img class="site-logo" src="img/ss_logoGrey_nav.png" alt="Shaw Scott: a digital marketing agency" /></a>
-		</div>
-		
-		<div id="site-nav" class="navbar-collapse collapse">
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="http://shawscott.com/sandBox/SS_Website_PRODUCTION/#capabilities" class="scrollto">CAPABILITIES</a></li>
-				<li><a href="labs">LABS</a></li>
-				<li><a href="portfolio">PORTFOLIO</a></li>
-				<li><a href="http://shawscott.com/sandBox/SS_Website_PRODUCTION/#team" class="scrollto">TEAM</a></li>
-				<li><a href="http://shawscott.com/sandBox/SS_Website_PRODUCTION/#resources" class="scrollto">RESOURCES</a></li>
-        <li><a href="http://www.shawscott.com/blog/">BLOG</a></li>
-        <li><a href="our_story">OUR STORY</a></li>
-				<li><a href="http://shawscott.com/sandBox/SS_Website_PRODUCTION/#contact" class="scrollto">CONTACT</a></li>
-			</ul>
-		</div>
+<div class="container-fluid">
+	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
+			<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
+		</button>
+		<a class="navbar-brand" href="#home"><img class="site-logo" src="img/ss_logoGrey_nav.png" alt="Shaw Scott: a digital marketing agency" /></a>
+	</div>
+	
+	<div id="site-nav" class="navbar-collapse collapse">
+		<ul class="nav navbar-nav navbar-right">
+			<li><a href="http://shawscott.com/sandBox/SS_Website_PRODUCTION/#capabilities" class="scrollto">CAPABILITIES</a></li>
+			<li><a href="labs">LABS</a></li>
+			<li><a href="portfolio">PORTFOLIO</a></li>
+			<li><a href="http://shawscott.com/sandBox/SS_Website_PRODUCTION/#team" class="scrollto">TEAM</a></li>
+			<li><a href="http://shawscott.com/sandBox/SS_Website_PRODUCTION/#resources" class="scrollto">RESOURCES</a></li>
+	        <li><a href="http://www.shawscott.com/blog/">BLOG</a></li>
+	        <li><a href="our_story">OUR STORY</a></li>
+			<li><a href="http://shawscott.com/sandBox/SS_Website_PRODUCTION/#contact" class="scrollto">CONTACT</a></li>
+		</ul>
 	</div>
 </div>
-=======
-		<div id="main-nav" class="navbar">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#site-nav">
-						<span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#home"><img class="site-logo" src="img/ss_logoGrey_nav.png" alt="Shaw Scott: a digital marketing agency" /></a>
-				</div>
-				
-				<div id="site-nav" class="navbar-collapse collapse">
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="http://shawscott.com#capabilities" class="scrollto">CAPABILITIES</a></li>
-						<li><a href="http://shawscott.com/labs">LABS</a></li>
-						<li><a href="http://shawscott.com/portfolio">PORTFOLIO</a></li>
-						<li><a href="http://shawscott.com#team" class="scrollto">TEAM</a></li>
-						<li><a href="http://shawscott.com#resources" class="scrollto">RESOURCES</a></li>
-                        <li><a href="http://www.shawscott.com/blog/">BLOG</a></li>
-                        <li><a href="http://shawscott.com/our_story">OUR STORY</a></li>
-						<li><a href="http://shawscott.com#contact" class="scrollto">CONTACT</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
->>>>>>> ef93f35cdf5854335d87d74fd3178cf35e8b7386
+</div>
